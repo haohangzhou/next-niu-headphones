21c848376a32087085ad72b245169199a5a8471c
Haohang Zhou <zhouhaohangau@gmail.com>
2022-06-19 20:49:12 +1000

    Added a footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { AiOutlineInstagram, AiOutlineLinkedin } from 'react-icons/ai';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className='footer-container'>
+			<p>Created by Joe Zhou</p>
+			<p className='icons'>
+				<AiOutlineInstagram
+					onClick={() => window.open('https://www.instagram.com/jiu0_0/')}
+				/>
+				<AiOutlineLinkedin
+					onClick={() => window.open('https://www.linkedin.com/in/joe-hz/')}
+				/>
+			</p>
+		</div>
+	);
 };
 
 export default Footer;
